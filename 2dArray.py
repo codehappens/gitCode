9dfc3b7b49b27055c61036f2c516d5a2853748b0
E Anatolios <anatolios007@gmail.com>
2018-08-07 21:58:31 -0700

    2 d Array problem on hackerrank solved https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

---
+++ 2dArray.py
@@ -0,0 +1,31 @@
+#!/bin/python 
+#https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    totals=[]
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            temp=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            totals.append(temp)
+    return max(totals)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in xrange(6):
+        arr.append(map(int, raw_input().rstrip().split()))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
